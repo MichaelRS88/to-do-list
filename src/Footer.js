73c27cd9c96ca542385a1e58daf078bf23804643
Michael R <michael.rubriche88@gmail.com>
2023-10-17 11:34:56 -0500

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+// Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      &copy; 2023 Michael Rubriche
+    </footer>
+  );
+}
+
+export default Footer;
